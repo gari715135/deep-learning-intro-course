b70424b7aa5f2a08da913a62adc47fa54ec28660
gari <giacomoaris@gmail.com>
2024-05-23 02:36:08 -0400

    nba

---
+++ sample_projects/nba/get_stats.py
@@ -0,0 +1,177 @@
+from nba_api.stats.static import teams, players
+from nba_api.stats.endpoints import cumestatsteamgames, cumestatsteam, gamerotation
+import pandas as pd
+import numpy as np
+import json
+import difflib
+import time
+import requests
+
+def retry(func, retries=3):
+    def retry_wrapper(*args, **kwargs):
+        attempts = 0
+        while attempts < retries:
+            try:
+                return func(*args, **kwargs)
+            except requests.exceptions.RequestException as e:
+                print(e)
+                time.sleep(30)
+                attempts += 1
+
+    return retry_wrapper
+
+def getSeasonScheduleFrame(seasons,seasonType): 
+
+    # Get date from string
+    def getGameDate(matchup):
+        return matchup.partition(' at')[0][:10]
+
+    # Get Home team from string
+    def getHomeTeam(matchup):
+        return matchup.partition(' at')[2]
+
+    # Get Away team from string
+    def getAwayTeam(matchup):
+        return matchup.partition(' at')[0][10:]
+
+    # Match nickname from schedule to team table to find ID
+    def getTeamIDFromNickname(nickname):
+        return teamLookup.loc[teamLookup['nickname'] == difflib.get_close_matches(nickname,teamLookup['nickname'],1)[0]].values[0][0] 
+    
+    @retry
+    def getRegularSeasonSchedule(season,teamID,seasonType):
+        season = str(season) + "-" + str(season+1)[-2:] # Convert year to season format ie. 2020 -> 2020-21
+        teamGames = cumestatsteamgames.CumeStatsTeamGames(league_id = '00',season = season ,
+                                                                      season_type_all_star=seasonType,
+                                                                      team_id = teamID).get_normalized_json()
+
+        teamGames = pd.DataFrame(json.loads(teamGames)['CumeStatsTeamGames'])
+        teamGames['SEASON'] = season
+        return teamGames    
+    
+    # Get team lookup table
+    teamLookup = pd.DataFrame(teams.get_teams())
+    
+    # Get teams schedule for each team for each season
+    scheduleFrame = pd.DataFrame()
+
+    for season in seasons:
+        for id in teamLookup['id']:
+            time.sleep(1)
+            scheduleFrame = scheduleFrame.append(getRegularSeasonSchedule(season,id,seasonType))
+            
+    scheduleFrame['GAME_DATE'] = pd.to_datetime(scheduleFrame['MATCHUP'].map(getGameDate))
+    scheduleFrame['HOME_TEAM_NICKNAME'] = scheduleFrame['MATCHUP'].map(getHomeTeam)
+    scheduleFrame['HOME_TEAM_ID'] = scheduleFrame['HOME_TEAM_NICKNAME'].map(getTeamIDFromNickname)
+    scheduleFrame['AWAY_TEAM_NICKNAME'] = scheduleFrame['MATCHUP'].map(getAwayTeam)
+    scheduleFrame['AWAY_TEAM_ID'] = scheduleFrame['AWAY_TEAM_NICKNAME'].map(getTeamIDFromNickname)
+    scheduleFrame = scheduleFrame.drop_duplicates() # There's a row for both teams, only need 1
+    scheduleFrame = scheduleFrame.reset_index(drop=True)
+            
+    return scheduleFrame
+
+def getSingleGameMetrics(gameID,homeTeamID,awayTeamID,awayTeamNickname,seasonYear,gameDate):
+
+    @retry
+    def getGameStats(teamID,gameID,seasonYear):
+        gameStats = cumestatsteam.CumeStatsTeam(game_ids=gameID,league_id ="00",
+                                               season=seasonYear,season_type_all_star="Regular Season",
+                                               team_id = teamID).get_normalized_json()
+
+        gameStats = pd.DataFrame(json.loads(gameStats)['TotalTeamStats'])
+
+        return gameStats
+
+    data = getGameStats(homeTeamID,gameID,seasonYear)
+    data.at[1,'NICKNAME'] = awayTeamNickname
+    data.at[1,'TEAM_ID'] = awayTeamID
+    data.at[1,'OFFENSIVE_EFFICIENCY'] = (data.at[1,'FG'] + data.at[1,'AST'])/(data.at[1,'FGA'] - data.at[1,'OFF_REB'] + data.at[1,'AST'] + data.at[1,'TOTAL_TURNOVERS'])
+    data.at[1,'SCORING_MARGIN'] = data.at[1,'PTS'] - data.at[0,'PTS']
+
+    data.at[0,'OFFENSIVE_EFFICIENCY'] = (data.at[0,'FG'] + data.at[0,'AST'])/(data.at[0,'FGA'] - data.at[0,'OFF_REB'] + data.at[0,'AST'] + data.at[0,'TOTAL_TURNOVERS'])
+    data.at[0,'SCORING_MARGIN'] = data.at[0,'PTS'] - data.at[1,'PTS']
+
+    data['SEASON'] = seasonYear
+    data['GAME_DATE'] = gameDate
+    data['GAME_ID'] = gameID
+
+    return data
+
+def getGameLogs(gameLogs,scheduleFrame):
+    
+    # Functions to prepare additional columns after gameLogs table loads
+    def getHomeAwayFlag(gameDF):
+        gameDF['HOME_FLAG'] = np.where((gameDF['W_HOME']==1) | (gameDF['L_HOME']==1),1,0)
+        gameDF['AWAY_FLAG'] = np.where((gameDF['W_ROAD']==1) | (gameDF['L_ROAD']==1),1,0) 
+
+    def getTotalWinPctg(gameDF):
+        gameDF['TOTAL_GAMES_PLAYED'] = gameDF.groupby(['TEAM_ID','SEASON'])['GAME_DATE'].rank(ascending=True)
+        gameDF['TOTAL_WINS'] = gameDF.sort_values(by='GAME_DATE').groupby(['TEAM_ID','SEASON'])['W'].cumsum()
+        gameDF['TOTAL_WIN_PCTG'] = gameDF['TOTAL_WINS']/gameDF['TOTAL_GAMES_PLAYED']
+        return gameDF.drop(['TOTAL_GAMES_PLAYED','TOTAL_WINS'],axis=1)
+
+    def getHomeWinPctg(gameDF):
+        gameDF['HOME_GAMES_PLAYED'] = gameDF.sort_values(by='GAME_DATE').groupby(['TEAM_ID','SEASON'])['HOME_FLAG'].cumsum()
+        gameDF['HOME_WINS'] = gameDF.sort_values(by='GAME_DATE').groupby(['TEAM_ID','SEASON'])['W_HOME'].cumsum()
+        gameDF['HOME_WIN_PCTG'] = gameDF['HOME_WINS']/gameDF['HOME_GAMES_PLAYED']
+        return gameDF.drop(['HOME_GAMES_PLAYED','HOME_WINS'],axis=1)
+
+    def getAwayWinPctg(gameDF):
+        gameDF['AWAY_GAMES_PLAYED'] = gameDF.sort_values(by='GAME_DATE').groupby(['TEAM_ID','SEASON'])['AWAY_FLAG'].cumsum()
+        gameDF['AWAY_WINS'] = gameDF.sort_values(by='GAME_DATE').groupby(['TEAM_ID','SEASON'])['W_ROAD'].cumsum()
+        gameDF['AWAY_WIN_PCTG'] = gameDF['AWAY_WINS']/gameDF['AWAY_GAMES_PLAYED']
+        return gameDF.drop(['AWAY_GAMES_PLAYED','AWAY_WINS'],axis=1)
+
+    def getRollingOE(gameDF):
+        gameDF['ROLLING_OE'] = gameDF.sort_values(by='GAME_DATE').groupby(['TEAM_ID','SEASON'])['OFFENSIVE_EFFICIENCY'].transform(lambda x: x.rolling(3, 1).mean())
+
+    def getRollingScoringMargin(gameDF):
+        gameDF['ROLLING_SCORING_MARGIN'] = gameDF.sort_values(by='GAME_DATE').groupby(['TEAM_ID','SEASON'])['SCORING_MARGIN'].transform(lambda x: x.rolling(3, 1).mean())
+
+    def getRestDays(gameDF):
+        gameDF['LAST_GAME_DATE'] = gameDF.sort_values(by='GAME_DATE').groupby(['TEAM_ID','SEASON'])['GAME_DATE'].shift(1)
+        gameDF['NUM_REST_DAYS'] = (gameDF['GAME_DATE'] - gameDF['LAST_GAME_DATE'])/np.timedelta64(1,'D') 
+        return gameDF.drop('LAST_GAME_DATE',axis=1)
+    
+    start = time.perf_counter_ns()
+
+    i = int(len(gameLogs)/2) #Can use a previously completed gameLog dataset
+
+    while i<len(scheduleFrame):
+
+        time.sleep(1)
+        gameLogs =  gameLogs.append(getSingleGameMetrics(scheduleFrame.at[i,'GAME_ID'],scheduleFrame.at[i,'HOME_TEAM_ID'],
+                         scheduleFrame.at[i,'AWAY_TEAM_ID'],scheduleFrame.at[i,'AWAY_TEAM_NICKNAME'],
+                         scheduleFrame.at[i,'SEASON'],scheduleFrame.at[i,'GAME_DATE']))
+        
+        gameLogs = gameLogs.reset_index(drop=True)
+
+        end = time.perf_counter_ns()
+
+        #Output time it took to load x amount of records
+        if i%100 == 0:
+            mins = ((end-start)/1e9)/60
+            print(i,str(mins) + ' minutes')
+
+        i+=1
+        
+    # Get Table Level Aggregation Columns
+    getHomeAwayFlag(gameLogs)
+    gameLogs = getHomeWinPctg(gameLogs)
+    gameLogs = getAwayWinPctg(gameLogs)
+    gameLogs = getTotalWinPctg(gameLogs)
+    getRollingScoringMargin(gameLogs)
+    getRollingOE(gameLogs)
+    gameLogs = getRestDays(gameLogs)
+
+    return gameLogs.reset_index(drop=True)
+
+#Get ScheduleFrame
+
+seasons = [2020]#,2021,2022]
+seasonType = 'Regular Season'
+
+start = time.perf_counter_ns() # Track cell's runtime
+scheduleFrame = getSeasonScheduleFrame(seasons,seasonType)
+end = time.perf_counter_ns()
+
